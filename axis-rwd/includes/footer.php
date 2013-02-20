f3dd171aa32c9734372673fcd204de2723fe97ce
Ed Wheeler <ewheeler3@gmail.com>
2013-02-19 20:44:52 -0500

    initial commit

---
+++ axis-rwd/includes/footer.php
@@ -0,0 +1,35 @@
+<div id="footer">
+  <ul>		
+	<li><a href="/">Home</a></li>
+	<li><a href="/about/">About</a></li>
+	<li><a href="/axis-road-hazard/">Axis Road Hazard Plans</a></li>
+	<li><a href="/contact/">Contact</a></li>	
+  </ul>
+
+  
+  <p>&copy; 2010 Axis Automotive Products. Site design by <a href="http://www.pelletized.com" target="_blank">pelletized.com</a>.</p>
+</div>	
+
+<!-- Start of StatCounter Code -->
+<script type="text/javascript">
+var sc_project=5884388; 
+var sc_invisible=1; 
+var sc_security="0fb66949"; 
+</script>
+
+<script type="text/javascript" src="http://www.statcounter.com/counter/counter_xhtml.js"></script><noscript><div class="statcounter"><a title="hit counter" class="statcounter" href="http://www.statcounter.com/free_hit_counter.html"><img class="statcounter" src="http://c.statcounter.com/5884388/0/0fb66949/1/" alt="hit counter" /></a></div></noscript>
+<!-- End of StatCounter Code -->
+
+<!-- google analytics -->
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-19759818-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+<!-- /google analytics -->
---
+++ axis-rwd/index.php
@@ -0,0 +1,89 @@
+<?php
+$pagetype = 'home'; 
+$pagetitle = 'Axis Automotive Products - Road Hazard Protection Plans';
+$keywords = 'Road, Hazard, Tire, wheel, cosmetic, curb, roadside, towing, flat, windshield, chip, dent, key, Pothole, warranty';
+$description = 'Road hazard plans that protect your tires and wheels, and more.';
+?>
+
+<?php include('includes/header.php'); ?>
+
+	<div id="main">	
+		<div class="col first">
+			<h1>AXIS ROAD HAZARD <span>Standard</span></h1>
+			<h2>Tire and Wheel Protection:</h2>
+			<img src="images/pothole.jpg" width="260" height="153" alt="pothole" class="featured-img" />
+			<h3>Flat Tire coverage:</h3>
+			<p>You will be reimbursed for charges incurred for repairs caused by road hazards.</p>
+			
+			<h3>Wheel/Rim Protection:</h3>
+			<p>You will be reimbursed for expenses arising out of the repair or replacement of wheels rendered unserviceable due to a road hazard.</p>
+			
+			<h3>Mounting, Balancing and Taxes:</h3>
+			<p>You will be reimbursed for the cost of mountinig and balancing of any tire covered under this agreement. You will also be reimbursed for any applicable local or state taxes that may be required as a part of the repair.</p>
+			
+			<h3>Limited Roadside Assistance:</h3>
+			<p>In the event of damage caused by a road hazard, we will dispatch help to assist in changing the spare tire. If no spare is available, we will tow your vehicle to the nearest available service station.</p>
+				
+		</div>	
+		<div class="col border-left">
+			<h1>AXIS ROAD HAZARD <span>Premium</span></h1>
+			<h2>Standard Protection - Plus:</h2>
+			<img src="images/roadassist.jpg" width="260" height="153" alt="roadside assistance" class="featured-img" />
+			<h3>Cosmetic and Curb Damage:</h3>
+			<p>In the event the face of your wheel becomes damaged from a road hazard or from curb damage, we will repair the damage for the length of your agreement.</p>
+
+			<h3>Rental Reimbursement:</h3>
+			<p>Should you need a rental car during the time we need to repair your wheel, we will reimburse you up to 3 days of rental at a maximum of $50 per day.</p>
+
+			<h3>24 Hour Full Roadside Assistance:</h3>
+			<p>Towing - up to 25 miles</p>
+
+			<h3>Emergency Road Service</h3>			
+			<ul>
+				<li><em>Essential Fluids and Supplies Delivery</em><br />
+					Includes delivery of gasoline, water, oil, or any other supplies necessary to get you on your way. We pay for the delivery and you pay for the cost of supplies.</li>
+				<li><em>Flat Tire Assistance</em><br />
+					We will change your flat tire with your good spare.</li>
+				<li><em>Emergency Battery Assistance</em><br />
+					All of our trucks are capable of giving you a jump start or tightening your cables as needed.</li>
+				<li><em>Lockout Service</em><br />
+					Whether you are locked out of your car or your keys are lost or damaged, we will send out a locksmith to help you out.</li>
+			</ul>		
+		</div>		
+		<div class="col border-left">
+			<h1>AXIS ROAD HAZARD <span>Platinum</span></h1>
+			<h2>Premium Protection - Plus:</h2>
+			<img src="images/keys.jpg" width="260" height="153" alt="keys" class="featured-img" />
+			<h3>Windshield Repair:</h3>
+			<p>Repair of the front windshield of chips and cracks caused by propelled rocks or road hazard debris while driving on public roads or highways. Will not increase your vehicle insurance premiums if used.</p>
+
+			<h3>Paintless Dent Repair:</h3>
+			<p>This process permanently removes door dings and minor dents without harming the vehicles factory finish.</p>
+<ul>
+	<li>No paint required</li>
+	<li>Permanent repair of dents and dings</li>
+	<li>Won't harm factory finish</li>
+	<li>No Hassle, easy to use</li>
+</ul>
+
+<h3>Key / Remote Replacement:</h3>
+<p>In the event your key is lost, stolen, or destroyed we will pay for a replacement key/remote.</p>
+<p><em>** Please see your contract for specific terms and conditions.</em></p>
+			<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+		</div>
+		
+
+
+		<div class="clear"></div>
+		
+	</div>
+	
+</div>	
+	<?php include('includes/footer.php'); ?>
+
+
+
+
+
+</body>
+</html>
