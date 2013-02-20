8a2d35c399d781ecc49811f20ddbbf39e748b343
Ed Wheeler <ewheeler3@gmail.com>
2013-02-19 20:29:24 -0500

    initial commit

---
+++ compare.php
@@ -0,0 +1,81 @@
+<?php include('includes/header.php'); ?>
+
+	<div id="main">	
+		<h1>Comparison Chart</h1>
+			
+		<table cellpadding="0" cellspacing="0" border="0" id="compare-chart">
+			<tr>
+				<th></th>
+				<th>Axis</th>
+				<th>Leading Industry<br /> Road Hazards</th>
+			<tr>
+			<tr>
+				<td width="175">Unlimited Coverage?</td>
+				<td width="50"><img src="images/check.jpg" width="24" height="24" alt="" /></td>
+				<td width="100"><img src="images/x.jpg" width="24" height="24" alt="" /></td>
+			</tr>
+			<tr>
+				<td>NO Aggregates?</td>
+				<td><img src="images/check.jpg" width="24" height="24" alt="" /></td>
+				<td><img src="images/x.jpg" width="24" height="24" alt="" /></td>
+			</tr>
+			<tr>
+				<td>Cover Curb Damage?</td>
+				<td><img src="images/check.jpg" width="24" height="24" alt="" /></td>
+				<td><img src="images/x.jpg" width="24" height="24" alt="" /></td>
+			</tr>
+			<tr>
+				<td>Cover Cosmetic Damage?</td>
+				<td><img src="images/check.jpg" width="24" height="24" alt="" /></td>
+				<td><img src="images/x.jpg" width="24" height="24" alt="" /></td>
+			</tr>
+			<tr>
+				<td>Cover Rental Reimbursement?</td>
+				<td><img src="images/check.jpg" width="24" height="24" alt="" /></td>
+				<td><img src="images/x.jpg" width="24" height="24" alt="" /></td>
+			</tr>
+			<tr>
+				<td>Choices of Protection?</td>
+				<td><img src="images/check.jpg" width="24" height="24" alt="" /></td>
+				<td><img src="images/x.jpg" width="24" height="24" alt="" /></td>
+			</tr>
+			<tr>
+				<td>available after point of sale?</td>
+				<td><img src="images/check.jpg" width="24" height="24" alt="" /></td>
+				<td><img src="images/x.jpg" width="24" height="24" alt="" /></td>
+			</tr>
+			<tr>
+				<td>FULL Roadside?</td>
+				<td><img src="images/check.jpg" width="24" height="24" alt="" /></td>
+				<td><img src="images/x.jpg" width="24" height="24" alt="" /></td>
+			</tr>
+			<tr>
+				<td>Dent/Ding Protection?</td>
+				<td><img src="images/check.jpg" width="24" height="24" alt="" /></td>
+				<td><img src="images/x.jpg" width="24" height="24" alt="" /></td>
+			</tr>
+			<tr>
+				<td>Windshield Chip Protection?</td>
+				<td><img src="images/check.jpg" width="24" height="24" alt="" /></td>
+				<td><img src="images/x.jpg" width="24" height="24" alt="" /></td>
+			</tr>
+			<tr>
+				<td>key &amp; Remote Replacement?</td>
+				<td><img src="images/check.jpg" width="24" height="24" alt="" /></td>
+				<td><img src="images/x.jpg" width="24" height="24" alt="" /></td>
+			</tr>
+		</table>
+        
+		<div class="clear"></div>
+		
+	</div>
+	
+</div>	
+	<?php include('includes/footer.php'); ?>
+
+
+
+
+
+</body>
+</html>
